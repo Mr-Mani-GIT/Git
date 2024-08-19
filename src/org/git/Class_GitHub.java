c84c323fad6fa4c5052bc7b61da613312d4f8330
Mr-Mani-GIT <manin6891@gmail.com>
2024-08-19 17:58:46 +0530

    GitHub

---
+++ src/org/git/Class_GitHub.java
@@ -0,0 +1,20 @@
+package org.git;
+
+public class Class_GitHub {
+	
+	
+	public void method1() {
+		System.out.println("The mangement system");
+	}
+	
+	public void method2() {
+		System.out.println("DataBase..");
+	}
+
+	public static void main(String[] args) {
+		Class_GitHub c = new Class_GitHub();
+		c.method1();
+		c.method2();
+	}
+
+}
